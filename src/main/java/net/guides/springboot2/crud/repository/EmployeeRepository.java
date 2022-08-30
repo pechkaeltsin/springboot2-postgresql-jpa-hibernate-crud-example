ac955911d502a18fc82f0dd98802058034359269
Fazylzyanov Timur <fazylzyanov@reksoft.ru>
2022-08-30 17:52:51 +0300

    Initial commit

---
+++ src/main/java/net/guides/springboot2/crud/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package net.guides.springboot2.crud.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import net.guides.springboot2.crud.model.Employee;
+
+
+/**
+ * <DESCRIPTION>.
+ *
+ * @author fazylzyanov@reksoft.ru
+ */
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
